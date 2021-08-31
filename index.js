25de312e87c8124a5f94aa922418be1ad12899ba
ryan77710 <lolliaryan@ryan-ordi.local>
2021-08-31 13:59:55 +0200

    add socket and initianlize server

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const app = require("./src/app");
-
 const mongoose = require("mongoose");
 
+//-------------
+// const app = require("./src/app");
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
+// app.listen(process.env.PORT || 3200, () => {
+//   console.log("server has started");
+// });
+
+const app = require("./src/app");
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-app.listen(process.env.PORT || 3200, () => {
+const httpServer = require("http").createServer(app);
+const options = {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+};
+const io = require("socket.io")(httpServer, options);
+let userPos = {};
+io.on("connection", (socket) => {
+  socket.on("login", (arg) => {
+    socket.emit("newUser", arg);
+  });
+});
+
+httpServer.listen(process.env.PORT || 3200, () => {
   console.log("server has started");
 });
